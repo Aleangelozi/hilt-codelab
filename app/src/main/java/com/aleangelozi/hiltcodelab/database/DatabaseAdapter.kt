e98536036d7fc22adc451512a6e2e15ffbe40ca7
aleangelozi <aleangelozi@gmail.com>
2022-04-15 17:24:40 -0300

    Providing Context

--- app/src/main/java/com/aleangelozi/hiltcodelab/database/DatabaseAdapter.kt
+++ app/src/main/java/com/aleangelozi/hiltcodelab/database/DatabaseAdapter.kt
@@ -1,12 +1,18 @@
 package com.aleangelozi.hiltcodelab.database
 
+import android.content.Context
 import android.util.Log
 import com.aleangelozi.hiltcodelab.TAG
+import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
 
-class DatabaseAdapter @Inject constructor(var databaseService: DatabaseService) {
+class DatabaseAdapter @Inject constructor(
+    @ActivityContext private val context: Context,
+    var databaseService: DatabaseService
+) {
     fun log(message: String) {
         Log.d(TAG, "DatabaseAdapter: $message")
         databaseService.log(message)
+        Log.d(TAG, "Context available: $context")
     }
 }
