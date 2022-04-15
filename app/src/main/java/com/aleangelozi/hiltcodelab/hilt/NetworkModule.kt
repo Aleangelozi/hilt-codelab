54a75f79fafcffd80f33107c8eb012f55d7dc106
aleangelozi <aleangelozi@gmail.com>
2022-04-15 15:48:16 -0300

    Hilt Module

---
+++ app/src/main/java/com/aleangelozi/hiltcodelab/hilt/NetworkModule.kt
@@ -0,0 +1,10 @@
+package com.aleangelozi.hiltcodelab.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class NetworkModule {
+}
